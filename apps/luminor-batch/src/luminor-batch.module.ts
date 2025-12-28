1ab811684672b755216c5e0045d09433e392f4e6
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2025-12-28 21:09:47 +0900

    feat: config env variablr

--- apps/luminor-batch/src/luminor-batch.module.ts
+++ apps/luminor-batch/src/luminor-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LuminorBatchController } from './luminor-batch.controller';
 import { LuminorBatchService } from './luminor-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [LuminorBatchController],
   providers: [LuminorBatchService],
 })
